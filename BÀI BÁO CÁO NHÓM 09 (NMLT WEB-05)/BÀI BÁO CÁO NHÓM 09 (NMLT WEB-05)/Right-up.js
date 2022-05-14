2b5f9549d8b767dd6d41dbfb151065b50fac702c
your name <nghiatran00932@gmail.com>
2022-05-14 11:19:06 +0700

    first commit

---
+++ BÀI BÁO CÁO NHÓM 09 (NMLT WEB-05)/BÀI BÁO CÁO NHÓM 09 (NMLT WEB-05)/Right-up.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#Right #Right-up').slick({
+        centerPadding: '5px',
+        slidesToShow: 2,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
+    });
+});
